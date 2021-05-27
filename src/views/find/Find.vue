f7edaa90b55a201086b3fee845ffef683d775952
Yxlhsx <1196965104@qq.com>
2021-05-27 18:02:34 +0800

    修改layout布局

--- src/views/find/Find.vue
+++ src/views/find/Find.vue
@@ -1,9 +1,13 @@
 <template>
-  <div>这里是发现页</div>
+  <div class="find">这里是发现页</div>
 </template>
 
 <script setup>
 </script>
 
 <style lang="less">
+.find {
+  min-height: 100vh;
+  background-color: #efefef;
+}
 </style>
--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -1,33 +1,35 @@
 <template>
-  <div class="header" @click="fullscreen()">
-    <div class="title">{{ pageTitle }}</div>
-    <div class="menu">
-      <div></div>
-      <div></div>
+  <div class="layout">
+    <div class="header" @click="fullscreen()">
+      <div class="title">{{ pageTitle }}</div>
+      <div class="menu">
+        <div></div>
+        <div></div>
+      </div>
     </div>
-  </div>
 
-  <div class="main">
-    <router-view />
-  </div>
+    <div class="main">
+      <router-view />
+    </div>
 
-  <div class="footer">
-    <router-link to="/home" class="nav-item" :class="{ selected: index === '/home' }">
-      <div class="item-ico"></div>
-      <span>微信</span>
-    </router-link>
-    <router-link to="/contact" class="nav-item" :class="{ selected: index === '/contact' }">
-      <div class="item-ico"></div>
-      <span>通讯录</span>
-    </router-link>
-    <router-link to="/find" class="nav-item" :class="{ selected: index === '/find' }">
-      <div class="item-ico"></div>
-      <span>发现</span>
-    </router-link>
-    <router-link to="/mine" class="nav-item" :class="{ selected: index === '/mine' }">
-      <div class="item-ico"></div>
-      <span>我</span>
-    </router-link>
+    <div class="footer">
+      <router-link to="/home" class="nav-item" :class="{ selected: index === '/home' }">
+        <div class="item-ico"></div>
+        <span>微信</span>
+      </router-link>
+      <router-link to="/contact" class="nav-item" :class="{ selected: index === '/contact' }">
+        <div class="item-ico"></div>
+        <span>通讯录</span>
+      </router-link>
+      <router-link to="/find" class="nav-item" :class="{ selected: index === '/find' }">
+        <div class="item-ico"></div>
+        <span>发现</span>
+      </router-link>
+      <router-link to="/mine" class="nav-item" :class="{ selected: index === '/mine' }">
+        <div class="item-ico"></div>
+        <span>我</span>
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -59,81 +61,77 @@ const fullscreen = () => {
 </script>
 
 <style lang="less" scoped>
-.header {
-  width: 100vw;
-  height: 50px;
-  background-color: #efefef;
-  position: fixed;
-  top: 0;
-  display: flex;
-  align-items: center;
-
-  .title {
-    margin: 0 auto;
-    font-weight: 600;
-  }
-
-  .menu {
-    width: 70px;
-    position: absolute;
-    right: 10px;
+.layout {
+  // padding: 50px 0 70px 0;
+  // box-sizing: border-box;
+  .header {
+    width: 100vw;
+    height: 50px;
+    background-color: #efefef;
+    position: fixed;
+    top: 0;
     display: flex;
-    justify-content: space-between;
-
-    & > div {
-      width: 30px;
-      height: 30px;
-      background-color: #010101;
-    }
-  }
-}
-
-.main {
-  margin: 50px 0 70px 0;
-}
-
-.footer {
-  width: 100vw;
-  height: 70px;
-  box-sizing: border-box;
-  border-top: #d8d8d8 solid 0.5px;
-  background-color: #f7f7f7;
-  position: fixed;
-  bottom: 0;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-
-  .nav-item {
-    width: 22%;
-    height: 70px;
-    display: flex;
-    flex-direction: column;
     align-items: center;
-    justify-content: center;
-    font-size: 14px;
-    text-decoration: none;
 
-    .item-ico {
-      width: 30px;
-      height: 30px;
-      background-color: #010101;
+    .title {
+      margin: 0 auto;
+      font-weight: 600;
     }
-    span {
-      color: #010101;
+
+    .menu {
+      width: 70px;
+      position: absolute;
+      right: 10px;
+      display: flex;
+      justify-content: space-between;
+
+      & > div {
+        width: 30px;
+        height: 30px;
+        background-color: #010101;
+      }
     }
   }
 
-  .router-link-active {
-    color: #f7f7f7;
+  .main {
+    margin: 50px 0 70px 0;
   }
-  // .selected {
-  //   .item-ico {
-  //     background-color: #10bb61;
-  //   }
-  //   span {
-  //     color: #10bb61;
-  //   }
-  // }
+
+  .footer {
+    width: 100vw;
+    height: 70px;
+    box-sizing: border-box;
+    border-top: #d8d8d8 solid 0.5px;
+    background-color: #f7f7f7;
+    position: fixed;
+    bottom: 0;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    .nav-item {
+      width: 22%;
+      height: 70px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      font-size: 14px;
+      text-decoration: none;
+
+      .item-ico {
+        width: 30px;
+        height: 30px;
+        background-color: #010101;
+      }
+      span {
+        color: #010101;
+      }
+    }
+
+    .router-link-active {
+      color: #f7f7f7;
+    }
+  }
 }
 </style>
