bab2e36ccfb81dd99311e506a472ecece8d4872e
Yxlhsx <yxlhsx@qq.com>
2022-07-11 11:21:40 +0800

    新增：聊天信息页面

--- src/router/index.js
+++ src/router/index.js
@@ -6,89 +6,98 @@ import Find from '@/views/find/Find.vue'
 import Mine from '@/views/mine/Mine.vue'
 
 const routes = [
-  {
-    path: '/',
-    redirect: '/home'
-  },
-  {
-    path: '/',
-    name: 'Layout',
-    component: Layout,
-    children: [
-      {
-        path: '/home',
-        name: 'Home',
-        meta: {
-          title: '微信',
-          headerType: 'tab'
-        },
-        component: Home
-      },
-      {
-        path: '/contact',
-        name: 'Contact',
-        meta: {
-          title: '通讯录',
-          headerType: 'tab'
-        },
-        component: Contact
-      },
-      {
-        path: '/find',
-        name: 'Find',
-        meta: {
-          title: '发现',
-          headerType: 'tab'
-        },
-        component: Find
-      },
-      {
-        path: '/mine',
-        name: 'Mine',
-        meta: {
-          title: '我',
-          headerType: 'empty'
-        },
-        component: Mine
-      },
-      {
-        path: '/chat',
-        name: 'Chat',
-        meta: {
-          title: '聊天',
-          headerType: 'page'
-        },
-        component: () => import('@/views/chat/Chat.vue')
-      },
-      {
-        path: '/setting',
-        name: 'Setting',
-        meta: {
-          title: '设置',
-          headerType: 'page'
-        },
-        component: () => import('@/views/setting/Index.vue')
-      },
-      {
-        path: '/circle_friends',
-        name: 'CircleFriends',
-        meta: {
-          title: '朋友圈',
-          headerType: 'page'
-        },
-        component: () => import('@/views/circle-friends/CircleFriends.vue')
-      }
-    ]
-  },
-  {
-    path: '/:pathMatch(.*)*', // 此处需特别注意置于最底部
-    redirect: '/home'
-  }
+    {
+        path: '/',
+        redirect: '/home'
+    },
+    {
+        path: '/',
+        name: 'Layout',
+        component: Layout,
+        children: [
+            {
+                path: '/home',
+                name: 'Home',
+                meta: {
+                    title: '微信',
+                    headerType: 'tab'
+                },
+                component: Home
+            },
+            {
+                path: '/contact',
+                name: 'Contact',
+                meta: {
+                    title: '通讯录',
+                    headerType: 'tab'
+                },
+                component: Contact
+            },
+            {
+                path: '/find',
+                name: 'Find',
+                meta: {
+                    title: '发现',
+                    headerType: 'tab'
+                },
+                component: Find
+            },
+            {
+                path: '/mine',
+                name: 'Mine',
+                meta: {
+                    title: '我',
+                    headerType: 'empty'
+                },
+                component: Mine
+            },
+            {
+                path: '/chat',
+                name: 'Chat',
+                meta: {
+                    title: '聊天',
+                    headerType: 'page'
+                },
+                component: () => import('@/views/chat/Chat.vue')
+            },
+            {
+                path: '/chat_info',
+                name: 'ChatInfo',
+                meta: {
+                    title: '聊天信息',
+                    headerType: 'empty'
+                },
+                component: () => import('@/views/chat/ChatInfo.vue')
+            },
+            {
+                path: '/setting',
+                name: 'Setting',
+                meta: {
+                    title: '设置',
+                    headerType: 'page'
+                },
+                component: () => import('@/views/setting/Index.vue')
+            },
+            {
+                path: '/circle_friends',
+                name: 'CircleFriends',
+                meta: {
+                    title: '朋友圈',
+                    headerType: 'page'
+                },
+                component: () => import('@/views/circle-friends/CircleFriends.vue')
+            }
+        ]
+    },
+    {
+        path: '/:pathMatch(.*)*', // 此处需特别注意置于最底部
+        redirect: '/home'
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+    history: createWebHistory(),
+    routes
 })
 
 export default router
---
+++ src/views/chat/ChatInfo.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="user-list">
+        <ul>
+            <li>
+                <img
+                    src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+                    alt="头像"
+                />
+                <p>工具人</p>
+            </li>
+            <li>
+                <img
+                    src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+                    alt="头像"
+                />
+                <p>工具人</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import { ref, computed, onMounted, reactive, nextTick } from 'vue'
+import { useRoute } from 'vue-router'
+
+import Button from '@/components/Button/index.vue'
+import Toast from '@/components/Toast/toast'
+
+const route = useRoute()
+</script>
+
+<style lang="less" scoped>
+@main-height: calc(100vh - 90rem - @footer-height);
+@footer-height: 120rem;
+
+.user-list {
+    width: 100%;
+    height: 240rem;
+    background-color: white;
+    padding: 0 40rem;
+}
+.user-list > ul {
+    display: flex;
+    align-items: center;
+    height: 100%;
+
+    li {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        width: 130rem;
+        height: 160rem;
+
+        img {
+            width: 120rem;
+            border-radius: 20rem;
+        }
+
+        p {
+            font-size: 24rem;
+        }
+    }
+
+    li:not(:last-child) {
+        margin-right: 40rem;
+    }
+}
+</style>
